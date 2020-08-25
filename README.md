5f7ce30f349e3fe9f374228fbe75f70582a7ad6d
Mriyam Tamuli <mbtamuli@gmail.com>
2020-08-25 22:05:57 +0530

    Add README and showip example
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Network Programming using Go
+
+Testing the waters of Network Programming with Go.
+Following this awesome guide - [Beej's Guide to Network Programming
+](https://beej.us/guide/bgnet/)
+
+Each directory contains the C code from the examples given in the guide, along with my implementation of the program in Go.
---
+++ showip/showip.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("usage:", os.Args[0], "hostname")
+		os.Exit(1)
+	}
+	name := os.Args[1]
+
+	addrs, err := net.LookupHost(name)
+	if err != nil {
+		fmt.Println("Resolution error", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("IP addresses for %s:\n\n", name)
+
+	for _, addr := range addrs {
+		ip := net.ParseIP(addr)
+		if ip.To4() == nil {
+			fmt.Printf("  %s: %s\n", "IPv6", ip)
+			continue
+		}
+		fmt.Printf("  %s: %s\n", "IPv4", ip)
+	}
+}
